841f6c57b284fc38dcfcf9556514354437658e57
Shreeram2k3 <kanishreeram@gmail.com>
2025-08-11 11:07:57 +0530

    Add SA_VII student activity model and controller
    Introduces SA_VIIController for handling student activity form submissions and file uploads. Adds SA_VII Eloquent model with file deletion logic. Updates migration to fix column names for 'Page_Nos' and 'Conference_Details'.

---
+++ app/Models/StudentsActivityModels/SA_VII.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models\StudentsActivityModels;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+
+class SA_VII extends Model
+{
+    protected $primaryKey='S_NO';
+    protected $table ="StudentActivity_7";
+    protected $fillable=[
+        'Month',
+        'Name_of_Student(s)',
+        'Roll_No',
+        'Staff(if_guided)',
+        'Title_of_the_Paper',
+        'Name_of_the_Journal',
+        'Volume_No',
+        'Page Nos',
+        'Conference Details',
+        'National/International',
+        'Dept',
+        'Document_Link',
+        'Document',
+        'user_id'
+    ];
+    public $timestamps=false;
+
+     protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($activity) {
+            if ($activity->Document && Storage::disk('public')->exists($activity->Document)) {
+                Storage::disk('public')->delete($activity->Document);
+            }
+        });
+    }
+
+}
