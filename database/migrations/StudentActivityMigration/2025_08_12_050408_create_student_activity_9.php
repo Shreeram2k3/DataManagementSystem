b6f77a2bb38159aa7ce0faf0b23c863e707040a1
Shreeram2k3 <kanishreeram@gmail.com>
2025-08-12 10:56:44 +0530

    stA9 migration file created

---
+++ database/migrations/StudentActivityMigration/2025_08_12_050408_create_student_activity_9.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('StudentActivity_9', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->string('Semester');
+            $table->string('Name_of_Course');
+            $table->string('Name_of_Faculty/Resource_Person');
+            $table->date('From_Date');
+            $table->date('To_Date');
+            $table->string('Value_Added/One_Credit');
+            $table->string('Coordinator');
+            $table->string('Dept');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('StudentActivity_9');
+    }
+};
