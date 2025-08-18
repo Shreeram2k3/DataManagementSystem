8c1ca4d6b5f202153c49dc8b0e545e25ad475b07
Shreeram2k3 <kanishreeram@gmail.com>
2025-08-18 10:20:15 +0530

    update SAX

--- resources/views/user/StudentActivityViews/StudentActivityForms/SA_X.blade.php
+++ resources/views/user/StudentActivityViews/StudentActivityForms/SA_X.blade.php
@@ -19,54 +19,63 @@
             <label class="block" for="name_of_the_student">
                     <span class="text-sm text-gray-600">Name of the Student</span>
                     <textarea name="name_of_the_student" id="name_of_the_student" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2" required 
-                    >{{ $record['Name_of_Course'] ?? old('Name_of_Course') }}</textarea>
+                    >{{ $record['Name_of_the_student'] ?? old('Name_of_the_student') }}</textarea>
             </label>
 
-            <label class="block">
-                    <span class="text-sm text-gray-600">Name of Faculty/Resource Person</span>
-                    <textarea name="name_of_faculty_resource_person" id="name_of_faculty_resource_person" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2"
+            <label class="block" for="roll_no">
+                    <span class="text-sm text-gray-600">Roll No</span>
+                    <textarea name="roll_no" id="roll_no" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2"
                      required>{{ $record->Roll_No ?? old('Roll_No') }}</textarea>
             </label>  
                     
-            <label class="block">
-                    <span class="text-sm text-gray-600">Name of the Organization</span>
+            <label class="block" for="from_date">
+                    <span class="text-sm text-gray-600">Date(from)</span>
                     
-                    <input  type="text" id="name_of_the_organization" name="name_of_the_organization" required  value="{{ $record['Name_of_the_Organization']?? old('Name_of_the_Organization') }}" 
+                    <input  type="date" id="from_date" name="from_date" required  value="{{ $record['From_Date']?? old('From_Date') }}" 
                     class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
             
-            <label class="block">
-                    <span class="text-sm text-gray-600">From Date</span>
 
-                    <input  type="date" id="from_date" name="from_date" required  value="{{ $record->From_Date ?? old('From_Date') }}" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
-            </label>
-
-            <label class="block">
-                    <span class="text-sm text-gray-600">To Date</span>
+            <label class="block" for="to_date">
+                    <span class="text-sm text-gray-600">Date(to)</span>
 
                     <input  type="date" id="to_date" name="to_date" required  value="{{ $record->To_Date ?? old('To_Date') }}" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
      
     
-            <label class="block">
-                    <span class="text-sm text-gray-600">Value Added/One Credit</span>
+            <label class="block" for="industry_details">
+                    <span class="text-sm text-gray-600">Industry Details</span>
                     
-                    <input  type="text" id="name_of_the_journal" name="name_of_the_journal" required value="{{ $record['Value_Added/One_Credit'] ?? old('Value_Added/One_Credit') }}" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
+                    <input  type="text" id="industry_details" name="industry_details" required value="{{ $record['Industry_Details'] ?? old('Industry_Details') }}" class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
 
-            <label class="block">
-                    <span class="text-sm text-gray-600">Coordinator</span>
+            <label class="block" for="stipend">
+                    <span class="text-sm text-gray-600">Stipend(Rs/Month)</span>
                     
-                    <input  type="text" id="coordinator" name="coordinator" required  value="{{ $record['Coordinator'] ?? old('Coordinator') }}" 
+                    <input  type="text" id="stipend" name="stipend" required  value="{{ $record['Stipend(Rs/Month)'] ?? old('Stipend(Rs/Month)') }}" 
                     class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
 
            
 
-            <label class="block">
-                    <span class="text-sm text-gray-600">Department</span>
+            <label class="block" for="nature_of_training">
+                    <span class="text-sm text-gray-600">Nature of Training</span>
                     
-                    <input  type="text" id="dept" name="dept" required  value="{{ $record['Dept'] ?? old('Dept') }}" 
+                    <input  type="text" id="nature_of_training" name="nature_of_training" required  value="{{ $record['Nature_of_Training'] ?? old('Nature_of_Training') }}" 
+                    class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
+            </label>
+
+            <label class="block" for="duration">
+                    <span class="text-sm text-gray-600">Duration</span>
+                    
+                    <input  type="text" id="duration" name="duration" required  value="{{ $record['Duration'] ?? old('Duration') }}" 
+                    class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
+            </label>
+
+            <label class="block" for="assessment">
+                    <span class="text-sm text-gray-600">Assessment</span>
+                    
+                    <input  type="text" id="assessment" name="assessment" required  value="{{ $record['Assessment'] ?? old('Assessment') }}" 
                     class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
 
@@ -74,14 +83,14 @@
 
 
 
-            <label class="block">
+            <label class="block" for="document_link">
                     <span class="text-sm text-gray-600">Document Link</span>
                     
                     <input type="url" id="document_link" name="document_link" value="{{ $record->Document_Link ?? old('Document_Link') }}" 
                     class="w-full border-b border-pink-400 focus:outline-none focus:border-pink-600 py-2">
             </label>
 
-            <label class="block">
+            <label class="block" for="document">
                     <span class="text-sm text-gray-600">Document</span>
 
                     {{-- Show existing document name or link if record exists --}}
@@ -106,3 +115,4 @@
                 {{ isset($record) ? 'Update' : 'Submit' }}
             </button>
 </form>
+</main>
