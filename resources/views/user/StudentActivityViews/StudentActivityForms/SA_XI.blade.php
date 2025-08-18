7bfd5d128a47c8d2efa1292d19a9b75d5df70fa8
Shreeram2k3 <kanishreeram@gmail.com>
2025-08-18 13:05:03 +0530

    update SA_XI bug fix

--- resources/views/user/StudentActivityViews/StudentActivityForms/SA_XI.blade.php
+++ resources/views/user/StudentActivityViews/StudentActivityForms/SA_XI.blade.php
@@ -90,6 +90,7 @@
                             Current file: 
                             <a href="{{ asset('storage/' . $record->Document) }}" class="text-blue-500 underline"target="blank">
                                 {{ basename($record->Document) }}
+                            </a>
                         </p>
                     @endif
 
