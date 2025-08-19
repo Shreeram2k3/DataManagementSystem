f4484d29aacea6ba06ce360b392f53912fc2c5be
Sathish21-cse <sathishdeepak49@gmail.com>
2025-08-19 16:17:36 +0530

    clean

--- app/Http/Controllers/StudentsActivityController/SA_XIIIController.php
+++ app/Http/Controllers/StudentsActivityController/SA_XIIIController.php
@@ -35,7 +35,7 @@ class SA_XIIIController extends Controller
 
             // set the user_id to the authenticated user's ID
             $validated['user_id'] = auth()->id();
-            // dd($validated);
+             //dd($validated);
 
              // to store the file in public 
             if ($request->hasFile('document')) {
