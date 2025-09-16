4f2180e48b8481dca582dc8a7ae120ce5bd27980
Shreeram2k3 <kanishreeram@gmail.com>
2025-09-16 12:26:23 +0530

    da3,da4 migrations

---
+++ database/migrations/DepartmentActivityMigration/2025_09_16_062905_create_department_activity_3.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DepartmentActivity_3', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->string('Total_Number_of_Titles');
+            $table->string('Total_Number_of_Books');
+            $table->string('Total_Number_of_Reference_Books');
+            $table->string('Total_Number_of_Journals_Subscribed_National');
+            $table->string('Total_Number_of_Journals_Subscribed_International');
+            $table->string('Total_Value_of_Books/Journals_Investment(National)');
+            $table->string('Total_Value_of_Books/Journals_Investment(international)');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DepartmentActivity_3');
+    }
+};
