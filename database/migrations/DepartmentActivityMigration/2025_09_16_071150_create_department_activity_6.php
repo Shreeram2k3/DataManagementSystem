ed69336c670bd3b0730965536889b3ed6d785139
Shreeram2k3 <kanishreeram@gmail.com>
2025-09-16 13:05:38 +0530

    da 6 to 11tables

---
+++ database/migrations/DepartmentActivityMigration/2025_09_16_071150_create_department_activity_6.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DepartmentActivity_6', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->date('Date');
+            $table->string('Duration');
+            $table->string('Activity');
+            $table->string('Speaker/Organization');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DepartmentActivity_6');
+    }
+};
---
+++ database/migrations/DepartmentActivityMigration/2025_09_16_072907_create_department_activity_10.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DepartmentActivity_10', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->string('AY-SEM');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DepartmentActivity_10');
+    }
+};
