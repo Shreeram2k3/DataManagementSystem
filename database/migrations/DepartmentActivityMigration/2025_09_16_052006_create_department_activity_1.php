52a9a589fea61395d318839164b7a712f3939566
Sathish21-cse <sathishdeepak49@gmail.com>
2025-09-16 11:09:27 +0530

    DA 1,2 migration are done

---
+++ database/migrations/DepartmentActivityMigration/2025_09_16_052006_create_department_activity_1.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DepartmentActivity_1', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->string('Details_of_Equipment');
+            $table->string('Number_of_Equipment');
+            $table->string('Name_of_Laboratory');
+            $table->string('Value_of_the_Equipment_Rs');
+            $table->string('Dept');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DepartmentActivity_1');
+    }
+};
---
+++ database/migrations/DepartmentActivityMigration/2025_09_16_053003_create_department_activity_2.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('DepartmentActivity_2', function (Blueprint $table) {
+            $table->id('S_NO');
+            $table->string('Name_of_the_Faculty');
+            $table->string('Name_of_the_Equipment_failed/ Serviced');
+            $table->string('Name_of_the_Lab');
+            $table->string('Servicing_details');
+            $table->string('Amount_Rs');
+            $table->string('status');
+            $table->string('Date');
+            $table->string('Dept');
+            $table->string('Document_Link',2083)->nullable();
+            $table->string('Document');
+            $table->foreignId('user_id')->constrained('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('DepartmentActivity_2');
+    }
+};
