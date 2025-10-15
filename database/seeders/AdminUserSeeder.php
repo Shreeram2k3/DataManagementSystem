5e9bd4e986844a2c4c9671f74dbe8f2ebfd420cb
Shreeram2k3 <kanishreeram@gmail.com>
2025-10-15 11:01:31 +0530

    update admin seeder
    now the 1st user is super_admin

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -15,7 +15,7 @@ class AdminUserSeeder extends Seeder
             [
                 'name' => env('ADMIN_NAME', 'Admin'),
                 'password' => Hash::make(env('ADMIN_PASSWORD', 'password123')),
-                'role' => 'admin',
+                'role' => 'super_admin',
                 'department' =>env('ADMIN_DEPARTMENT', 'CSE'),
             ]
         );
