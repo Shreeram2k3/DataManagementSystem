d51112c1cd146802375b3626c76f834923774880
Shreeram2k3 <kanishreeram@gmail.com>
2025-10-17 15:07:28 +0530

    Add conditional display for Action column in tables
    The Action column and its contents in student activity tables are now shown only if the $showActions variable is true. This improves flexibility for rendering tables with or without action controls based on context.

--- resources/views/user/StudentActivityViews/StudentActivityTables/SA_XIITable.blade.php
+++ resources/views/user/StudentActivityViews/StudentActivityTables/SA_XIITable.blade.php
@@ -5,7 +5,9 @@
                 <th class="px-4 py-3 border">Dept</th>
                 <th class="px-4 py-3 border">Document Link </th>
                 <th class="px-4 py-3 border">Document</th>
+                @if($showActions)
                 <th class="px-4 py-3 border">Action</th>
+                @endif
     </tr>
 </thead>
 
@@ -30,7 +32,8 @@
                       </a>
                   </td>
 
-<td class="px-4 py-2 border text-center">
+                  @if($showActions)
+                <td class="px-4 py-2 border text-center">
                     <div class="flex justify-center rounded-lg overflow-hidden">
         
                     <!-- Edit Button -->
@@ -52,6 +55,7 @@
                     </form>
                 </div>
                 </td>
+                @endif
             </tr>
             @empty
                 <tr>
